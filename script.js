1b0d6b9c4f51b914aa7874b7d8c2b4aa9fd2f3c4
Kamran <kamran-anwar99@hotmail.com>
2022-03-10 22:25:34 +0000

    Search functionality completed

--- script.js
+++ script.js
@@ -15,12 +15,33 @@ let weather = { /*storing the finctions and variables for API*/
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed)
-        document.querySelector(".city").innerText = "Weather in" + name;
+        // console.log(name, icon, description, temp, humidity, speed) //No longer being tested
+        document.querySelector(".city").innerText = "The weather in " + name;
         document.querySelector(".icon").src =
             "https://openweathermap.org/img/wn/" + icon + ".png"
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "°C";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(" .search-bar").value);
     }
 };
 
+document.querySelector(" .search button")
+    .addEventListener("click", function () {
+        weather.search();
+    });
+
+
+document.querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+
+weather.fetchWeather("London");
 // https://api.openweathermap.org/data/2.5/weather?lat=0.3032&lon=51.5069&units=metric&appid=da4a6ce5038464971b0767a4956221ba
-//lat=0.3032&lon=51.5069
